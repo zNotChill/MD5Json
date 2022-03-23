05a6e5afa443d1c466d861c4c10896ba2a001f9c
BlockArchitech <hello@blockarchitech.com>
2022-03-23 18:38:30 -0400

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+# bootstrap for MDJson
+
+
+
+# Cloning Repository
+echo "Cloning Repository..."
+git clone https://github.com/zNotChill/MDJson.git && cd MDJson
+
+# NPM Update (Future Use)
+echo "Updating MDJson and Dependencies"
+npm update
+
+echo "MDJson has been cloned and updated. You're currently in the MDJson Directory."
+echo "Before contributing please make sure to read the code of conduct."
