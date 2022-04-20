1349a29598bdc8f1cc0368eee0c1d3be0c795b41
zNotChill <59340653+zNotChill@users.noreply.github.com>
2022-04-20 11:04:23 +0100

    Update v1.js

--- v1.js
+++ v1.js
@@ -1,17 +1,14 @@
 
 
-function mdjson(s) {
+const mdjson = function(s) { 
 	if (!s) {
 		throw new TypeError("String not defined");
 	} else {
-		function split(s) {
-			var u = s.split(' ');
-			var r = [];
-			for (let l = 0; l < u.length; l++) {
-				r.push(`${l}: {"split": "${s[l]}","md5": "${md5(s[l])}"`)
-			}
-			return r.join('},')
+		var u = s.split(' ');
+		var r = [];
+		for (let l = 0; l < u.length; l++) {
+			r.push(`${l}: {"split": "${s[l]}","md5": "${md5(s[l])}"`)
 		}
-		return split(s)
+		return r.join('},')
 	}
 }
