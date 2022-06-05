c69e8d8774533529eebe46b62fdff56d9e336ba5
zNotChill <59340653+zNotChill@users.noreply.github.com>
2022-06-05 20:24:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
-
-const mdjson = function(s) { 
+const mdjson = function(s) {
 	var u = s.split(' ');
 	var r = [];
-	u.forEach((l) => {
+	for (let l = 0; l < u.length; l++) {
 		r.push(`${l}: {"split": "${s[l]}","md5": "${md5(s[l])}"`)
-	})
+	}
 	return r.join('},')
 }
+
 module.exports = {
 	mdjson
 }
