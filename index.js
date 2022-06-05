d4e94799e32f09423712cc5d01468da857f77852
zNotChill <59340653+zNotChill@users.noreply.github.com>
2022-06-05 20:21:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 
 
 const mdjson = function(s) { 
-	if (!s) {
-		throw new TypeError("String not defined");
-	} else {
-		var u = s.split(' ');
-		var r = [];
-		for (let l = 0; l < u.length; l++) {
-			r.push(`${l}: {"split": "${s[l]}","md5": "${md5(s[l])}"`)
-		}
-		return r.join('},')
-	}
-}
+	var u = s.split(' ');
+	var r = [];
+	u.forEach((l) => {
+		r.push(`${l}: {"split": "${s[l]}","md5": "${md5(s[l])}"`)
+	})
+	return r.join('},')
 
 module.exports = {
 	mdjson
